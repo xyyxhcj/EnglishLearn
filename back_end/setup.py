104d4806c4a9173f7179f351b6ae994c258e3243
hcj <xyyxhcj@qq.com>
2021-10-18 22:24:05 +0800

    fix: add setup.py

---
+++ back_end/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='EnglishLearn',
+    version='',
+    packages=['back_end'],
+    url='',
+    license='',
+    author='CJ',
+    author_email='xyyxhcj@qq.com',
+    description=''
+)
